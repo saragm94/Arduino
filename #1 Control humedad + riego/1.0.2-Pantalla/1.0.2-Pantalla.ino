eac76c1b1f385d12efa11736f6812b3712d112ee
Sara <saraglezm94@gmail.com>
2026-08-26 17:03:18 +0100

    Añadiendo la pantalla

--- #1 Control humedad + riego/1.0.2-Pantalla/1.0.2-Pantalla.ino
+++ #1 Control humedad + riego/1.0.2-Pantalla/1.0.2-Pantalla.ino
@@ -2,11 +2,15 @@
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  // pines pantalla (RS, E, D4, D5, D6, D7)
 
 const int sensorPin = A0;     //Pin
-const int valorSeco = 620;    // Tierra seca
-const int valorHumedo = 310;  // Sensor en agua
+const int valorSeco = 310;    // Tierra seca
+const int valorHumedo = 620;  // Sensor en agua
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2); // Tamaño pantalla
+  lcd.print("Iniciando...");
+  delay(1500);
+  lcd.clear();
 }
 
 void loop() {
@@ -14,11 +18,18 @@ void loop() {
   int porcentaje = map(lectura, valorSeco, valorHumedo, 0, 100); //Pasamos a porcentaje
   
   porcentaje = constrain(porcentaje, 0, 100); //Controlamos que no sea más de 100
-  
+  lcd.print("Iniciando...");
   //Muestra mensajes por terminal
   Serial.print("Humedad: ");
   Serial.print(porcentaje);
   Serial.println("%");
+
+  //Muestra mensajes por pantalla
+  lcd.setCursor(0, 0);      // Columna 0, fila 0 (primera línea)
+  lcd.print("Humedad:      "); // Los espacios en blanco borran texto viejo más largo
+  lcd.setCursor(9, 0);      // Nos movemos a la posición donde va el número
+  lcd.print(porcentaje);
+  lcd.print("%");
   
   delay(1000);
 }
